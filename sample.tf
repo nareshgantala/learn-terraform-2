e26d9f506c91c7cab1c75590666c0f05dd9dd0c5
nareshgantala <nareshgantala@gmail.com>
2024-10-30 16:38:08 +0530

    fix

--- sample.tf
+++ sample.tf
@@ -5,7 +5,7 @@ data "aws_ami" "example" {
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.example
+  ami           = data.aws_ami.example.image_id
   instance_type = "t3.micro"
 
   tags = {
