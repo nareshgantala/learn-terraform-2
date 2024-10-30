bbc5ecc6853080105e7eece148d3d126be896570
nareshgantala <nareshgantala@gmail.com>
2024-10-30 16:37:11 +0530

    push

--- sample.tf
+++ sample.tf
@@ -1,5 +1,11 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0b4f379183e5706b9"
+  ami           = data.aws_ami.example
   instance_type = "t3.micro"
 
   tags = {
@@ -7,6 +13,8 @@ resource "aws_instance" "web" {
   }
 }
 
+
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
+
